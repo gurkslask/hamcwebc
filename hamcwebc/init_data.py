31b33aebadf478f639498b1be49f21f708f4242b
Alexander Svensson <gurkslask@gmail.com>
2016-10-12 21:57:25 +0200

    Worked on initalization

--- hamcwebc/init_data.py
+++ hamcwebc/init_data.py
@@ -1,32 +1,44 @@
 """Loads the data points configuration from a import JSON file."""
 import json
+import os
 
 from .extensions import db
-from .database import Sensor
+from .database import Sensor, SensorLimit
 
 
-class InitializeSQL(object):
+class InitSQL(object):
     """Takes JSON and puts into SQL database."""
 
     def __init__(self):
         """Constructor."""
         self.file_name = 'data_points.json'
+        self.app_dir = os.path.abspath(os.path.dirname(__file__))
 
     def read_file(self):
         """Extract the JSON file."""
-        with open(self.file_name, 'r') as f:
+        with open(os.path.join(self.app_dir, self.file_name), 'r') as f:
             self.jsonData = json.load(f)
             return self.jsonData
 
     def make_sql_insert(self, sql_values):
         """Take SQL_values and make an INSERT statement."""
         for i in sql_values:
-            print(i)
-            print('INSERT INTO sensor Name: {}, Value: {}'.format(
-                sql_values[i]['name'], sql_values[i]['value'])
-            )
             for j in sql_values[i]['limits']:
+                if not SensorLimit.query.filter_by(name=[j]['name']).first():
+                    db.session.add(SensorLimit(
+                        name=[j]['name'],
+                        value=[j]['value']
+                        )
+                    )
+                else:
+                    print('{} Already exists'.format(name=[j]['name']))
                 print(j)
+            print(Sensor.query.filter_by(name=sql_values[i]['name']).first())
+            if not Sensor.query.filter_by(name=sql_values[i]['name']).first():
+                db.session.add(Sensor(name=sql_values[i]['name'], value=sql_values[i]['value']))
+                db.session.commit()
+            else:
+                print('{} Already exists'.format(sql_values[i]['name']))
 
     def do_work(self):
         """Tie together everything."""
@@ -35,5 +47,5 @@ class InitializeSQL(object):
 
 
 if __name__ == '__main__':
-    a = InitializeSQL()
+    a = InitSQL()
     print(a.do_work())
