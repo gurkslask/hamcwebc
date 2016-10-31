1a1e2d71b7541316e17ecc78060917bfb7b64cfa
Alexander Svensson <gurkslask@gmail.com>
2016-10-31 20:45:30 +0100

    Started integrating sqlalchemy to the celery tasks

--- hamcwebc/tasks.py
+++ hamcwebc/tasks.py
@@ -1,6 +1,7 @@
 """Delay functions with celery."""
 from .extensions import celery
 from .connect_to_socket import call_server
+from .database import Sensor
 
 
 @celery.task(name='example_add')
@@ -12,4 +13,6 @@ def add_together(a, b):
 @celery.task(name='connect_to_pi')
 def connect_to_pi(message):
     """Connect to the pi and request or write data."""
-    return(call_server(message))
+    result = call_server(message)
+    a = Sensor(name=result.keys(), value=result[result.keys]['PV'])
+    print(a)
