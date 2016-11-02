ecd656b424b80b0437cb8ef07b9219be74649a43
Alexander Svensson <gurkslask@gmail.com>
2016-11-02 22:01:09 +0100

    Got sql  connection working from tasks

--- hamcwebc/tasks.py
+++ hamcwebc/tasks.py
@@ -1,7 +1,10 @@
 """Delay functions with celery."""
-from .extensions import celery
+from .extensions import celery, db
 from .connect_to_socket import call_server
-from .database import Sensor
+try:
+    from .database import Sensor
+except ImportError:
+    from hamcwebc.database import Sensor
 
 
 @celery.task(name='example_add')
@@ -14,5 +17,6 @@ def add_together(a, b):
 def connect_to_pi(message):
     """Connect to the pi and request or write data."""
     result = call_server(message)
-    a = Sensor(name=result.keys(), value=result[result.keys]['PV'])
-    print(a)
+    for key in result.keys():
+        db.session.add(Sensor(name=key, value=result[key]['PV']))
+    db.session.commit()
