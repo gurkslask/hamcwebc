c232de4701f52a7520702cd224c33211f8d170bc
Alexander Svensson <gurkslask@gmail.com>
2017-02-14 06:46:30 -0800

    Modified settings to include parameters

--- hamcwebc/settings.py
+++ hamcwebc/settings.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
 
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'  # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@IP:port/database'  # TODO: Change me
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
